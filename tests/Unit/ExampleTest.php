705719b84c0077f111602c33dbb95bb74a4710b8
Ace <ace@Aceui-MacBookPro.local>
2023-08-14 23:42:35 +0900

    test added

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-
+//use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
@@ -12,5 +12,26 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_if_this_works()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_if_this_admin_get_a_successful_response()
+    {
+        $response = $this->get('/admin/auth/login');
+
+        $response->assertStatus(200);
     }
 }
